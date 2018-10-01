159f05728e87bc091ded07e951afdd92647f6041
Jonathan Chavez <jonachavez@paypal.com>
2018-10-01 14:47:28 -0600

    Adding dependency injection

---
+++ app/src/main/java/com/xoom/countryflags/catalog/di/CatalogComponent.java
@@ -0,0 +1,7 @@
+package com.xoom.countryflags.catalog.di;
+
+import dagger.Component;
+
+@Component(modules = CatalogModule.class)
+public interface CatalogComponent {
+}
---
+++ app/src/main/java/com/xoom/countryflags/catalog/di/CatalogModule.java
@@ -0,0 +1,7 @@
+package com.xoom.countryflags.catalog.di;
+
+import dagger.Module;
+
+@Module
+public class CatalogModule {
+}
