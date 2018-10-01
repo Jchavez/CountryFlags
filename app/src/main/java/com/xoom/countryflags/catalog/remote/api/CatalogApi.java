a20505f3acc101199ecd55668e8165a05683cbc7
Jonathan Chavez <jonachavez@paypal.com>
2018-10-01 14:48:50 -0600

    Adding API to repository

--- app/src/main/java/com/xoom/countryflags/catalog/remote/api/CatalogApi.java
+++ app/src/main/java/com/xoom/countryflags/catalog/remote/api/CatalogApi.java
@@ -3,10 +3,9 @@ package com.xoom.countryflags.catalog.remote.api;
 import com.xoom.countryflags.catalog.remote.datasource.PaginationCountryRequestResource;
 
 import retrofit2.http.GET;
-import retrofit2.http.Query;
 
 public interface CatalogApi {
 
     @GET("v2/countries")
-    PaginationCountryRequestResource getCountries(@Query("page_size") int pageSize);
+    PaginationCountryRequestResource getCountries();
 }
--- app/src/main/java/com/xoom/countryflags/catalog/repository/CatalogRepository.java
+++ app/src/main/java/com/xoom/countryflags/catalog/repository/CatalogRepository.java
@@ -1,5 +1,17 @@
 package com.xoom.countryflags.catalog.repository;
 
+import com.xoom.countryflags.catalog.remote.api.CatalogApi;
+import com.xoom.countryflags.catalog.remote.datasource.CountryRequestResource;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
 public class CatalogRepository {
 
+    @Inject CatalogApi catalogApi;
+
+    private List<CountryRequestResource> getCountries() {
+        return catalogApi.getCountries().getItems();
+    }
 }
